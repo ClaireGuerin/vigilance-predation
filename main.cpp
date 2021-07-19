daabc211408d315bda4a413ad31ce03f59903118
ClaireGuerin <guerin.claire01@gmail.com>
2021-07-19 12:17:32 +0200

    individual coordinates generated different each time: check

--- main.cpp
+++ main.cpp
@@ -19,5 +19,18 @@ int main() {
     cout    << "Created indiv and placed on grid of size 10, at coordinates: "
             << coord.x << ", " << coord.y << "\n";
 
+    Individual myInd2(10);
+    Coord coord2 = myInd2.getCoordinates();
+
+    cout    << "Created indiv and placed on grid of size 10, at coordinates: "
+            << coord2.x << ", " << coord2.y << "\n";
+
+    Individual myInd3(10);
+    Coord coord3 = myInd3.getCoordinates();
+
+    cout    << "Created indiv and placed on grid of size 10, at coordinates: "
+            << coord3.x << ", " << coord3.y << "\n";
+
+
     return 0;
 }
