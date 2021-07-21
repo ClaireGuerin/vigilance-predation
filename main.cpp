51fd93d3592fa0ceebee6f971884153cf0180d0f
ClaireGuerin <guerin.claire01@gmail.com>
2021-07-21 15:35:46 +0200

    How to save individual values of changing length in a single file, e.g. vigilance level for each individual at each generation?

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <fstream>
 #include "src/Utils.h"
 #include "src/Individual.h"
 #include "src/Grid.h"
@@ -20,18 +21,26 @@ int main()
     ind.set_random_coord(param.edgeSize * param.edgeSize, reng);
   }
 
+/*  std::ofstream ofs("vigilance.txt"); 
+  if(!ofs.is_open()) {
+    std::cerr << "error: unable to open vigilance file\n";
+    exit(EXIT_FAILURE);
+  }*/
+
   for (size_t gen = 0; gen < param.nGen; ++gen) {
 
     if (pop.size() == 0) {
       std::cout << "Population extinct. Exiting...\n";
       break;
     } else {
+
       // ECOLOGICAL TIME STEPS
       for (size_t step = 0; step < param.ecoTime; ++step) {
+
         std::cout << "eco time step " << step << "\n";
         auto shares = pop.ecologicalStep(param, reng, resources);
 
-        // deplete and grow resources accordingly
+        // DEPLETE AND GROW RESOURCES
         // resourceConsumption = 1 - self.efficiency * shares
         // resourceGrowth = self.grid.resources * self.growth
         // limit growth of resources arbitrarily set to 200 / fecundity
@@ -39,7 +48,9 @@ int main()
           double tmpRes = resources.read(cell) * param.rGrowth * (1 - param.eff * shares.read(cell));
           resources.write(cell, 
                           (tmpRes > (200 / param.fecundity)) ? param.initResources : tmpRes);
+
         }
+
       }
 
       // EVOLUTIONARY TIME STEP
@@ -47,9 +58,11 @@ int main()
       std::cout << "Reproduction...\n";
       pop.evolutionaryStep(param, reng);
 
+      //ofs << pop.vigilance << "\n";
+
     }
+
   }
   
-
   return 0;
 }
