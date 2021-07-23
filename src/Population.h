b19407727fd196fe069736269825d3f5c72fd36e
ClaireGuerin <guerin.claire01@gmail.com>
2021-07-23 12:32:13 +0200

    update population

--- src/Population.h
+++ src/Population.h
@@ -10,110 +10,112 @@
 
 namespace vigi {
 
-    class Population 
-    {
-        public:
-            Population(const Parameter &);
-            size_t size() {return size_; };
+  class Population 
+  {
+    public:
+      Population(const Parameter &);
+      size_t size() const {return individuals_.size(); };
 
-            template <typename RENG>
-            void place(const Parameter & par, RENG& reng) {
-                for (auto& ind : individuals_) {
-                    ind.set_random_coord(par.edgeSize, reng);
-                }
-            }
+      template <typename RENG>
+      void place(const Parameter & par, RENG& reng) 
+      {
+        for (auto& ind : individuals_) 
+        {
+          ind.set_random_coord(par.edgeSize, reng);
+        }
+      }
 
-            size_t size() const { return individuals_.size(); }
+      size_t size() const { return individuals_.size(); }
 
-            template <typename RENG>
-            grd::Grid<double> ecologicalStep(const Parameter& param, RENG& reng, grd::Grid<double> resources) {
+      template <typename RENG>
+      Grid<double>& ecologicalStep(const Parameter& param, RENG& reng, const Grid<double>& resources) 
+      {
+        individualsExplore(param, reng);
+        individualsGatherAndSurvive(param, resources, reng);
+        return shares_;
+      }
 
-                return individualsGatherAndSurvive( param, 
-                                                    individualsExplore(param, reng),
-                                                    resources,
-                                                    reng);
+      template <typename RENG>
+      void evolutionaryStep(const Parameter& param, RENG& reng) 
+      {
 
-            }
+        std::vector<Individual> offspring;
 
-            template <typename RENG>
-            void evolutionaryStep(const Parameter& param, RENG& reng) {
+        for (auto& ind : individuals_) 
+        {
+          ind.reproduce(param, reng);
 
-                std::vector<Individual> offspring;
+          for (int offs = 0; offs < ind.nOffspring_; ++offs) 
+          {
+            Individual newIndiv = ind;
+            newIndiv.cleanSlate();
+            newIndiv.mutate(param, reng);
+            offspring.push_back(newIndiv);
+          }
+        }
 
-                for (auto& ind : individuals_) {
-                    ind.reproduce(param, reng);
+        individuals_ = offspring;
+      }
 
-                    for (int offs = 0; offs < ind.nOffspring_; ++offs) {
-                        Individual newIndiv = ind;
-                        newIndiv.cleanSlate();
-                        newIndiv.mutate(param, reng);
-                        offspring.push_back(newIndiv);
-                    }
-                }
+    private:
 
-                individuals_ = offspring;
-            }
+      struct vigil_abund_t {
+        double vigil;
+        size_t abund;
+      };
 
-        private:
-            size_t size_;
-            std::vector<Individual> individuals_;
+      std::vector<Individual> individuals_;
+      Grid<vigil_abund_t> vigidance_;
+      Grid<double> shares_;
+      
 
-            template <typename RENG>
-            std::pair< grd::Grid<double>, grd::Grid<size_t> > 
-                individualsExplore(const Parameter& param, RENG& reng) {
+      template <typename RENG>
+      std::pair< grd::Grid<double>, grd::Grid<size_t> > 
+      individualsExplore(const Parameter& param, RENG& reng) 
+      {
 
-                    // set abundance & vigilance to zero before each exploration
-                    auto vigil = grd::Grid<double>{param.edgeSize * param.edgeSize, 0.0};
-                    auto abund = grd::Grid<size_t>{param.edgeSize * param.edgeSize, 0};
+        // set abundance & vigilance to zero before each exploration
+        vigil_abund_t reset {0.0, 0};
+        vigidance.assign(reset);
 
-                    for (auto& ind : individuals_) {
-                        if (ind.isAlive_) {
-                            ind.explore(param, reng);
-                            vigil.write(ind.coordinates_.x, 
-                                        ind.coordinates_.y, 
-                                        vigil.read(ind.coordinates_.x, ind.coordinates_.y) + ind.vigilance_);
-                            abund.write(ind.coordinates_.x, 
-                                        ind.coordinates_.y, 
-                                        abund.read(ind.coordinates_.x, ind.coordinates_.y) + 1);
-                        }
-                    }
-
-                    return {vigil, abund};
-                }
+        for (auto& ind : individuals_) {
+          if (ind.isAlive_)
+          {
+            ind.explore(param, reng);
+            vigidance_.(ind.coordinates_).vigil += ind.vigilance_;
+            ++vigidance_.(ind.coordinates_).abund;
+          }
+        }
+      }
             
-            template <typename RENG>
-            grd::Grid<double> individualsGatherAndSurvive(   const Parameter& param, 
-                                                std::pair< grd::Grid<double>, grd::Grid<size_t> > vigidance,
-                                                grd::Grid<double> resources,
-                                                RENG& reng) {
-                // calculate share of resources
-                auto shares = grd::Grid<double>{param.edgeSize * param.edgeSize, 0.0};
-
-                for (size_t cell = 0; cell < (param.edgeSize * param.edgeSize); ++cell) {
-                    // share = SUM(1-v_i)/(gamma*n), gamma: competition parameter
-                    double shareCell = (vigidance.second.read(cell) - vigidance.first.read(cell)) / (param.competition * vigidance.second.read(cell));
-                    shares.write(cell, shareCell);
-                }
-
-                
-                for (auto& ind : individuals_) {
-                    if (ind.isAlive_) {
-                        // individuals gather resources
-                        ind.gather( param, 
-                                    resources.read(ind.coordinates_.x, ind.coordinates_.y), 
-                                    shares.read(ind.coordinates_.x, ind.coordinates_.y));
-                        // individuals survive or get predated upon
-                        ind.survive(param, reng);
-                    }
-                }
-
-                return shares;
-            }
+      template <typename RENG>
+      void individualsGatherAndSurvive(const Parameter& param, 
+                                                    const grd::Grid<double>& resources,
+                                                    RENG& reng) 
+      {
+        // calculate share of resources
+        for (size_t cell = 0; cell < shares_.size(); ++cell) {
+          // share = SUM(1-v_i)/(gamma*n), gamma: competition parameter
+          const auto& vd = vigidance_[cell];
+          shares_[cell] = (vd.vigil - vd.abund) / (param.competition * vd.abund);
+        }
+     
+        for (auto& ind : individuals_) {
+          if (ind.isAlive_) 
+          {
+            // individuals gather resources
+            ind.gather( param, resources(ind.coordinates_), shares_(ind.coordinates) );
+            // individuals survive or get predated upon
+            ind.survive(param, reng);
+          }
+        }
+      }
     };
 
-    Population::Population(const Parameter& param) :
-        size_(param.popSize),
-        individuals_(param.popSize, Individual{param})
+    inline Population::Population(const Parameter& param) :
+      individuals_(param.popSize, Individual{param}),
+      vigidance_(param.edgeSize),
+      shares_(param.edgeSize)
     {
     }
 }
