d44232193cc68ecd0e60152419f20f49e4256fc6
ClaireGuerin <guerin.claire01@gmail.com>
2021-08-02 16:01:01 +0200

    Simulation class

---
+++ src/Simulation.h
@@ -0,0 +1,14 @@
+#ifndef VIGI_SIMULATION_H
+#define VIGI_SIMULATION_H
+
+namespace vigi 
+{
+    class Simulation
+    {
+        public:
+            Simulation();
+        private:
+    };
+}
+
+#endif
